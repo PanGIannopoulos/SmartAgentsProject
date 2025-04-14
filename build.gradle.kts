891d88d51a36b8fae5ac5588d4031e1336a7038d
Panagis Gian <Panagisgiann@gmail.com>
2025-04-14 17:19:26 +0300

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("io.github.jason-lang:jason-interpreter:3.2.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import jason.JasonException;
+import jason.infra.local.RunLocalMAS;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            RunLocalMAS.main(new String[] { "agents-folder/FirstAgent.mas2j" });
+        } catch (JasonException e) {
+            e.printStackTrace();
+        }
+    }
+}
