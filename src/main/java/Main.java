f6ee9bb727e062490c1a2e587c347b360f954a66
Luminous <Panabougian@gmail.com>
2025-04-27 01:33:34 +0300

    Implemented A star on the agent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import jason.JasonException;
 import jason.infra.local.RunLocalMAS;
-import env.GridEnvironment;
 
 
 
@@ -9,13 +8,7 @@ import env.GridEnvironment;
 public class Main {
     public static void main(String[] args) {
 
-        // Δημιουργία του περιβάλλοντος και αρχικοποίηση
-//        GridEnvironment env = new GridEnvironment();
-//        env.init(new String[] {});  // Αρχικοποίηση του περιβάλλοντος με στόχους και εμπόδια
-//
-//        // Εκτύπωση αρχικής κατάστασης του περιβάλλοντος
-//        env.updatePercepts();
-//        env.printGrid();
+
 
 
         // Εκτέλεση του πράκτορα που έχει υλοποιηθεί με το ASL
